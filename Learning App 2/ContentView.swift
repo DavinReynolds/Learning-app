72e2cd4dcbfee0c8d772644c6e73fac18da5dff8
me <@me>
2021-12-02 08:40:13 -0600

    Initial Commit

---
+++ Learning App 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning App 2
+//
+//  Created by AM Scratch - Davin Reynolds on 12/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning App 2/Learning_App_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_App_2App.swift
+//  Learning App 2
+//
+//  Created by AM Scratch - Davin Reynolds on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_App_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
